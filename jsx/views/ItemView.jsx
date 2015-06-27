e5ac7e5eae0e5d56a4271a65d34096d475cce707
sheodox <imagamer27@msn.com>
2015-06-27 14:22:06 -0500

    removed a probably unneeded model event listener

--- jsx/views/ItemView.jsx
+++ jsx/views/ItemView.jsx
@@ -3,13 +3,6 @@ module.exports = (function() {
     var React = require('react');
 
     return React.createClass({
-        componentDidMount: function() {
-            //re-render on model updates
-            this.props.model.on(
-                'change',
-                this.forceUpdate.bind(this)
-            );
-        },
         render: function() {
             var text = this.props.model.get('text');
             return (
