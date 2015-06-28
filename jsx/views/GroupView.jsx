8ce6527af16dd242c5110e92025decb031f9d69c
sheodox <imagamer27@msn.com>
2015-06-28 14:09:42 -0500

    added adding lists

--- jsx/views/GroupView.jsx
+++ jsx/views/GroupView.jsx
@@ -13,22 +13,51 @@ module.exports = (function() {
             var lists = this.props.model.getLists(),
                 columnClass = '';
 
-            if (lists.length === 2) {
-                columnClass = 'col-md-6';
-            }
-            else if (lists.length > 2) {
-                columnClass = 'col-md-4';
-            }
+            if (lists.length) {
+                if (lists.length === 2) {
+                    columnClass = 'col-md-6';
+                }
+                else if (lists.length > 2) {
+                    columnClass = 'col-md-4';
+                }
 
-            return lists.map(function(list, index) {
-                //to be used as a base for unique IDs for panels so we can have unique ids for the bootstrap collapses
-                var uniqueId = 'list-' + index + '-';
+                return lists.map(function (list, index) {
+                    //to be used as a base for unique IDs for panels so we can have unique ids for the bootstrap collapses
+                    var uniqueId = 'list-' + index + '-';
 
+                    return (
+                        <div className={columnClass}>
+                            <List key={index} model={list} uniqueId={uniqueId}/>
+                        </div>
+                    )
+                });
+            }
+            else {
+                //show a message instead of lists if there aren't any lists
                 return (
-                    <div className={columnClass}>
-                        <List key={index} model={list} uniqueId={uniqueId} />
+                    <div className='jumbotron'>
+                        <h2>There aren't any lists here</h2>
+                        <button className='btn btn-primary icon-button' onClick={this.add}>
+                            <i className='fa fa-plus'></i>
+                            Add a list
+                        </button>
                     </div>
                 )
+            }
+        },
+        add: function() {
+            var self = this;
+            vex.dialog.open({
+                message: 'New List',
+                input: `<label for='dialog-text'></label>
+                        <input type='text' id='dialog-text' name='name' />`,
+                buttons: [
+                    $.extend({}, vex.dialog.buttons.YES, {text: 'Save'}),
+                    $.extend({}, vex.dialog.buttons.NO, {text: 'Cancel'})
+                ],
+                callback: function(data) {
+                    self.props.model.add(data);
+                }
             });
         },
         render: function() {
@@ -36,8 +65,18 @@ module.exports = (function() {
 
             return (
                 <div className='container-fluid'>
-                    <h1>{this.props.model.get('name')}</h1>
-                    {lists}
+                    <div className='panel panel-default'>
+                        <div className='panel-heading'>
+                            <h1 className='inline'>{this.props.model.get('name')}</h1>
+                            <button className='btn btn-default icon-button pull-right' onClick={this.add}>
+                                <i className='fa fa-plus'></i>
+                                Add a list
+                            </button>
+                        </div>
+                        <div className='panel-body'>
+                            {lists}
+                        </div>
+                    </div>
                 </div>
             );
         }
