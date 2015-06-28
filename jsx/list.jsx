d984eeb1d1b2d9b3b57914045fa0c75f7f1d9c7b
sheodox <imagamer27@msn.com>
2015-06-28 16:03:12 -0500

    added saving data

--- jsx/list.jsx
+++ jsx/list.jsx
@@ -19,16 +19,29 @@
         fs = require('fs'),
         ListAppView = require('./js/views/AppView'),
         AppModel = require('./models/AppModel'),
+        dataPath = path.join(gui.App.dataPath, 'data.json'),
         data;
 
     try {
-        data = JSON.parse(fs.readFileSync('./example-data.json').toString());//JSON.parse(fs.readFileSync(path.join(gui.App.dataPath, 'data.json')).toString());
+        //example data
+        //data = JSON.parse(fs.readFileSync('./example-data.json').toString());
+
+        //actual data
+        data = JSON.parse(fs.readFileSync(dataPath).toString());
     }
     catch(e) {
         data = []
     }
 
     var listAppModel = new AppModel(data);
+
+    //save data when anything changes
+    listAppModel.on('save', function() {
+        var dataToStore = listAppModel.toJSON();
+
+        //save
+        fs.writeFile(dataPath, JSON.stringify(dataToStore));
+    });
 
     React.render(
         <ListAppView model={listAppModel} />,
